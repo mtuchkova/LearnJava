e056668c6538e52668106fe22531ff3f04a69d62
mtuchkova <tuchusya@gmail.com>
2020-01-12 22:23:21 +0200

    all l4lev1ex3

---
+++ src/javaStart/l4lev1ex3.java
@@ -0,0 +1,16 @@
+package javaStart;
+
+public class l4lev1ex3 {
+    public static void main(String[] args) {
+//        Напечатайте таблицу умножения на 5. предпочтительно печатать 1 x 5 = 5, 2 x 5 = 10
+
+        int result = 1;
+
+        for (int i = 0; i <= 10; i++) {
+            result = 5 * i;
+            System.out.println(i + " * 5 = " + result);
+        }
+
+
+    }
+}
