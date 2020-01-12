af82ca5eabdcc37dd432b5e652d899ddf46ff318
mtuchkova <tuchusya@gmail.com>
2020-01-12 21:49:55 +0200

    all l4lev1ex1

---
+++ src/javaStart/l4lev1ex1.java
@@ -0,0 +1,36 @@
+package javaStart;
+
+import java.util.Scanner;
+
+public class l4lev1ex1 {
+    public static void main(String[] args) {
+
+// С помощью циклов нарисовать «обои». Причем количество полос
+// должно вводиться с клавиатуры. В примере 7 полос.
+//***+++***+++***+++***
+//***+++***+++***+++***
+//***+++***+++***+++***
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<n;j++){
+                if (j%2==0) {
+                    System.out.print("***");
+                } else {
+                    System.out.print("пусто");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
+
+
+
+
