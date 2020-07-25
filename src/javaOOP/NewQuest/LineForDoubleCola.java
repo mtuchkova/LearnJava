4821b5809c10e3c61eb3a100042fdf1238db709f
MTuchkova <Mariya.Tuchkova@alfabank.kiev.ua>
2020-07-25 18:07:13 +0300

    back

--- src/javaOOP/NewQuest/LineForDoubleCola.java
+++ src/javaOOP/NewQuest/LineForDoubleCola.java
@@ -1,25 +1,25 @@
-package javaOOP.NewQuest;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public  class LineForDoubleCola {
-
-    private  List<String> queueForDoubleCola = new ArrayList<>(){{add("Sheldon");add("Leonard");add("Volovitc");add("Kutrapalli");add("Penny");}};
-
-    public  List<String> getQueueForDoubleCola() {
-        return queueForDoubleCola;
-    }
-
-
-
-    public static List<String> changeQueueForDoubleCola(List<String> queueForDoubleCola,int a) {
-
-
-
-        return queueForDoubleCola;
-    }
-
-
-}
-
+//package javaOOP.NewQuest;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public  class LineForDoubleCola {
+//
+//    private  List<String> queueForDoubleCola = new ArrayList<>(){{add("Sheldon");add("Leonard");add("Volovitc");add("Kutrapalli");add("Penny");}};
+//
+//    public  List<String> getQueueForDoubleCola() {
+//        return queueForDoubleCola;
+//    }
+//
+//
+//
+//    public static List<String> changeQueueForDoubleCola(List<String> queueForDoubleCola,int a) {
+//
+//
+//
+//        return queueForDoubleCola;
+//    }
+//
+//
+//}
+//
